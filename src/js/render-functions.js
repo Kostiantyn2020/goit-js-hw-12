0e985eab942dc22fea9c9c2a4df95791b42ecc96
Kostiantyn2020 <161701157+Kostiantyn2020@users.noreply.github.com>
2024-08-03 14:08:25 +0300

    5.3

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,39 +1,3 @@
-/*export function renderImageCards(images) {
-  return images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `
-    <div class="photo-card">
-      <a href="${largeImageURL}">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      </a>
-      <div class="info">
-        <p><b>Likes:</b> ${likes}</p>
-        <p><b>Views:</b> ${views}</p>
-        <p><b>Comments:</b> ${comments}</p>
-        <p><b>Downloads:</b> ${downloads}</p>
-      </div>
-    </div>
-  `
-    )
-    .join('');
-}
-
-export function clearGallery() {
-  document.querySelector('.gallery').innerHTML = '';
-}
-
-export function appendImagesToGallery(markup) {
-  document.querySelector('.gallery').insertAdjacentHTML('beforeend', markup);
-}*/
-
 export function renderImageCards(images) {
   return images
     .map(
@@ -69,3 +33,4 @@ export function clearGallery() {
 export function appendImagesToGallery(markup) {
   document.querySelector('.gallery').insertAdjacentHTML('beforeend', markup);
 }
+
