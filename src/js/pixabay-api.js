56e71f2ae63fdcfd549bff389f23f677afc33811
Kostiantyn2020 <161701157+Kostiantyn2020@users.noreply.github.com>
2024-08-03 13:57:56 +0300

    5.1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,22 @@
-import axios from 'axios';
+/*import axios from 'axios';
 
 const API_KEY = '45081240-5f1794f19be8a159ba53eac07'; 
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImages(query, page = 1, perPage = 15) {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error('Network response was not ok');
+  }
+}*/
+
+import axios from 'axios';
+
+const API_KEY = 'YOUR_API_KEY';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query, page = 1, perPage = 15) {
